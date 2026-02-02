d56f0d490e36fce920556955b6a7f7aecd928106
anilpatil9948-cpu <anilpatil9948@gmail.com>
2026-02-02 16:00:33 +0530

    Add files via upload

---
+++ cigars.C
@@ -0,0 +1,21 @@
+// Online C compiler to run C program online
+#include <stdio.h>
+void main()
+{
+    int you,date;
+    printf("enter the ratg of you date\n");
+    scanf("%d %d",&you,&date);
+    if(you<=2||date<=2)
+    {
+        printf("0");
+    }
+    else if(you>=8||date<=8)
+    {
+        printf("2");
+    }
+    else
+    {
+        printf("1");
+    }
+}
+    
